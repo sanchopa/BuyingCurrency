385df773ccb6f948154c116c15ecf148d5058038
Alex <sanchopa@mail.ru>
2016-12-25 18:39:07 +0600

    Научился пользоваться properties

---
+++ src/test/java/PropertiesServiceTest.java
@@ -0,0 +1,19 @@
+import com.luxoft.buying_currency.service.PropertiesService;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import java.util.Properties;
+
+/**
+ * Created by User on 25.12.2016.
+ */
+public class PropertiesServiceTest {
+    private static final Logger log = Logger.getLogger(UserServiceTest.class);
+    @Test
+    public void testGetProperties() {
+        PropertiesService propServ = new PropertiesService();
+        Properties property = propServ.getProperty("src/main/resources/defaultUser.properties");
+        log.info(property.getProperty("MMM1"));
+
+    }
+}
